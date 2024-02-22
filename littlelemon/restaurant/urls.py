a4bbee45fbe3526dc761cd44cf32b0ed286a1cfb
Matthew, Reynolds <matt_rey@me.com>
2024-02-21 23:42:00 -0600

    Added permissions and token gen

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import say_hello, index, MenuItemView, SingleMenuItemView
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('hello/', say_hello, name='Hello'),
     path('', index, name='index'),
     path('menu/', MenuItemView.as_view()),
     path('menu/<int:pk>', SingleMenuItemView.as_view()),
+    path('api-token-auth/', obtain_auth_token),
 ]
