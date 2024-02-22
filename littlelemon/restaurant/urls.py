248ea151dbafb778bae2b7467f88c102731f7039
Matthew, Reynolds <matt_rey@me.com>
2024-02-21 22:22:42 -0600

    Added booking api

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import say_hello, index
+from .views import say_hello, index, MenuItemView, SingleMenuItemView
 
 urlpatterns = [
     path('hello/', say_hello, name='Hello'),
-    path('', index, name='index')
+    path('', index, name='index'),
+    path('menu/', MenuItemView.as_view()),
+    path('menu/<int:pk>', SingleMenuItemView.as_view()),
 ]
